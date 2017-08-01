a480d46113f1feb993da922c3ca32433d99b025b
faizan <faizan.khan.iitbhu@gmail.com>
2017-08-01 23:04:28 +0530

    changes for R CMD checks

--- R/plot.r
+++ R/plot.r
@@ -142,9 +142,9 @@ print.ggplot <- function(x, newpage = is.null(vp), vp = NULL, ...) {
   # Record dependency on 'ggplot2' on the display list
   # (AFTER grid.newpage())
   grDevices::recordGraphics(
-    requireNamespace("ggplot2", quietly = TRUE),
+    requireNamespace("ggplot2Animint", quietly = TRUE),
     list(),
-    getNamespace("ggplot2")
+    getNamespace("ggplot2Animint")
   )
 
   data <- ggplot_build(x)
