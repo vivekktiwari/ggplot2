56d7add2404d482874149d28f33fe72d7ffb473d
vivekktiwari <kr.vivek.ab@gmail.com>
2018-07-14 19:33:24 +0530

    theme is a_theme in test unit

--- tests/testthat/test-ggplot2.R
+++ tests/testthat/test-ggplot2.R
@@ -1,14 +1,14 @@
 context("ggplot2Compatibility")
 
 test_that("animint2 works fine with ggplot2", {
-  
+
   aa <- a_plot(mpg, aes(displ, hwy))
   gg <- ggplot2::ggplot(mpg, aes(displ, hwy))
-  
+
   expect_identical(aa$data, gg$data)
   expect_identical(aa$plot_env, gg$plot_env)
-  expect_identical(aa$theme, gg$theme)
-  
+  expect_identical(aa$a_theme, gg$theme)
+
   l1 <- aa$mapping[1]
   l2 <- gg$mapping[1]
   expect_equal(l1, l2)
