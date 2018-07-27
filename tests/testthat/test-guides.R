20d857667e061d9ef1bcae8d019efb68305809cf
vivekktiwari <kr.vivek.ab@gmail.com>
2018-07-27 15:29:46 +0530

    guide and guides is now a_guide and a_guides, guides_* and guide_* functions are now a_guides_* and a_guide_*

--- tests/testthat/test-guides.R
+++ tests/testthat/test-guides.R
@@ -1,10 +1,10 @@
 context("Guides")
 
 test_that("colourbar trains without labels", {
-  g <- guide_colorbar()
+  g <- a_guide_colorbar()
   sc <- a_scale_colour_continuous(limits = c(0, 4), labels = NULL)
 
-  out <- guide_train(g, sc)
+  out <- a_guide_train(g, sc)
   expect_equal(names(out$key), c("colour", ".value"))
 })
 
